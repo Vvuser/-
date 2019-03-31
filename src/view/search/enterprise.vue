66830a1cb8a8a48e7ca9fa77d76dbec66c974e86
weiwei <614330288@qq.com>
2019-03-31 12:41:32 +0800

    搜索框

--- src/view/search/enterprise.vue
+++ src/view/search/enterprise.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>
-    
+  <div class="enterprise">
+    <div class="dHeader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="item"></div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"企业信息"
+  data() {
+    return {
+      searchText: ""
+    }
+  },
+  methods:{
+    init(){
+      this.searchText = sessionStorage.getItem("searchText")
+    },
+  },
+  created() {
+    this.init()
+  }
 }
 </script>
 
-<style scoped>
+<style scope type="text/scss" lang="scss">
+  .enterprise{
+    width: 1200px;
+    margin: 0 auto;
+    .dHeader{
+      margin: 20px 30px 0 30px;
+      background: #ececf4;
+      height: 40px;
+      display: flex;
+      align-items: center;
+    }
+    .dHeader>div:nth-child(1){
+      width: 180px;
+      
+    }
+  }
 </style>
--- src/view/search/patent.vue
+++ src/view/search/patent.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "专利数据"
+  
 }
 </script>
 
