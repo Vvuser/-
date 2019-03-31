7cf6fb747745dcaef03f30b41131fb4fff4f5f70
wozhenhaokan <baiyangxiaoye@gmail.com>
2019-03-31 17:17:15 +0800

    企业详情index

---
+++ src/components/pagination.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-pagination
+        background
+        layout="prev, pager, next"
+        :total=total>
+    </el-pagination>
+</template>
+
+<script>
+  export default {
+    name: "",
+    props: ["total"],
+  }
+</script>
+
+<style scoped>
+
+</style>
