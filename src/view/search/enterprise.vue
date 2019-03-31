bfe48f4f01e76c1667a19bc5df3bbfd50f3e475d
weiwei <614330288@qq.com>
2019-03-31 15:48:30 +0800

    合并代码

--- src/view/search/enterprise.vue
+++ src/view/search/enterprise.vue
@@ -7,10 +7,18 @@
       <div>成立年限</div>
     </div>
     <div class="item">
-      <div></div>
-      <div>公司信息</div>
-      <div>注册资金</div>
-      <div>成立年限</div>
+      <div>
+        <img src="../../assets/image/collect.jpg"/>
+        <img src="../../assets/logo.png">
+      </div>
+      <div>
+        <h3>武安科前生物</h3>
+        <p>法定人代表：xxxx</p>
+        <p>电话：xxxxx 邮箱：xxxxx</p>
+        <p>地址：xxxxxx</p>
+      </div>
+      <div>3000万人民币</div>
+      <div>2001-11-1</div>
     </div>
   </div>
 </template>
@@ -46,6 +54,7 @@ export default {
   .enterprise{
     width: 1200px;
     margin: 0 auto;
+    background: #ffffff;
     .dHeader{
       margin: 20px 30px 0 30px;
       background: #ececf4;
@@ -65,14 +74,39 @@ export default {
     }
     .item{
       display: flex;
-      margin: 20px 30px 0 30px;
+      padding: 28px 0;
+      margin: 0 30px;
       align-items: center;
+      background: white;
+      color: #838895;
+      border-bottom: 1px solid #ececf5;
     }
     .item>div:nth-child(1){
       width: 180px;
+      display: flex;
+      align-items: center;
+      img:nth-child(1) {
+        margin: 0 10px;
+        width: 20px;
+        height: 20px;
+      }
+      img:nth-child(2) {
+        width: 120px;
+        height: 120px;
+        background: black;
+
+      }
     }
     .item>div:nth-child(2){
       width: 570px;
+      h3{
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 20px;
+      }
+      p{
+        font-size: 14px;
+      }
     }
     .item>div:nth-child(3){
       width: 270px;
--- src/view/search/publicOpinion.vue
+++ src/view/search/publicOpinion.vue
@@ -1,14 +1,97 @@
 <template>
-  <div>
-    
+  <div class="publicOpinion">
+    <div class="box">
+      <div class="item">
+        <h3>子啊顺道收个发货到是</h3>
+        <p>www.iasaas.com</p>
+        <p>基地啊师姐殴打教案设计的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都的撒娇哦多久啊是京东i阿瑟将军打死经济案件到死见到就是动静啊送佛阿森纳都撒娇案件到时间东京阿松i的骄傲松井大嵩i发爱的静安寺哦降低哦就扫</p>
+        <div class="i_footer">
+          <div>
+            <span>中文</span>
+            <span>矿业</span>
+            <span>工业</span>
+          </div>
+          <div>
+            <img v-show="false" src="../../assets/image/collect3.jpg">
+            <img src="../../assets/image/collect4.jpg">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {mapMutations, mapGetters} from 'vuex'
 export default {
-  
+  data() {
+    return {
+      searchText: ""
+    }
+  },
+  computed: {
+      ...mapGetters([
+        'getSeacherText'
+      ])
+  },
+  watch:{
+    getSeacherText() {
+      console.log(this.getSeacherText)
+    }
+  },
+  methods:{
+    /**
+     * 获取检索信息
+     */
+    getPOList() {
+      this.$get('/patent/invoke').then(res => {
+        console.log(res)
+      })
+    }
+  },
+  created() {
+
+  }
 }
 </script>
 
-<style scoped>
+<style scoped type="text/scss" lang="scss">
+.publicOpinion{
+  margin-top: 20px;
+  width: 1200px;
+  margin: 0 auto;
+  background: #ffffff;
+  .box{
+    margin-top: 20px;
+    .item{
+      margin: 0 30px;
+      padding: 20px 0;
+      border-bottom: 1px solid #edecf1;
+      color: #838895;
+      h3{
+        font-size: 16px;
+        font-weight: bold;
+      }
+      p:nth-child(2){
+        color: #cad0dd;
+        font-size: 12px;
+        margin: 10px 0;
+      }
+      p:nth-child(3){
+        font-size: 14px;
+        overflow:hidden; 
+        text-overflow:ellipsis;
+        display:-webkit-box; 
+        -webkit-box-orient:vertical;
+        -webkit-line-clamp:2;
+        margin-bottom: 15px;
+      }
+      div{
+        display: flex;
+        justify-content: space-between;
+
+      }
+    }
+  }
+}
 </style>
