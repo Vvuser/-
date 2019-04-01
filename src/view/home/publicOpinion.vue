3bf3b9bbc5730c8bfc386e369a4da87b2408b1b6
weiwei <614330288@qq.com>
2019-04-01 19:25:13 +0800

    首页 舆情信息

--- src/view/home/publicOpinion.vue
+++ src/view/home/publicOpinion.vue
@@ -2,70 +2,122 @@
   <div>
     <div class="enteprisebody">
       <div class="entepriseheart">
-        <span class="enterpriseSc" @click="can()">我的收藏</span>
+        <span class="enterpriseSc" @click="isshow=!isshow">我的收藏</span>
         <span class="enterpriseLL">|</span>
-        <span class="enterpriseLs" @click="to()">浏览历史</span>
+        <span class="enterpriseLs" @click="isshow=!isshow">浏览历史</span>
       </div>
-      <div class="entepriserenal">
-        <div class="entepriserenalsource">
-          <span class="entepriserenalsource-in">公司信息</span>
-          <span class="entepriserenalsource-to">注册资金</span>
-          <span class="entepriserenalsource-auto">成立年限</span>
-        </div>
-        <div class="entepriserenalall">
-          <div class="entepriserenalallTB" v-show="isshow">img</div>
-          <div class="entepriserenalallnm">img</div>
-          <div class="entepriserenalallWuHa">
-            <div>
-              <h1 class="entepriserenalallWuHa-nav">武汉科前开发股份有限公司</h1>
-            </div>
-            <div class="entepriserenalallFe">
-              <span>法定代表人:陈焕春</span>
-              <br>
-              <span>电话:9237828</span>
-              <br>
-              <span>邮箱:38128931@qq.com</span>
-              <br>
-              <span>地址:武汉市东湖新技术开发区高新二路419号</span>
-            </div>
+      <div class="publicOpinion">
+    <div class="box">
+      <div class="item" v-for="(item,index) in list" :key="index">
+        <h3 @click="href(item.url)">{{item.titleZh}}</h3>
+        <p>{{item.domain}}&nbsp;&nbsp;&nbsp;{{item.created}}</p>
+        <p v-html="item.abstractZh"></p>
+        <div class="i_footer">
+          <div>
+            <span>{{item.languageTname}}</span>
+            <span class="bord" v-for="(c_item,index) in item.keywordsZh" :key="index">{{c_item}}</span>
           </div>
-          <div class="entepriserenalallZhican">
-            <span>36000万人民币</span>
-          </div>
-          <div class="entepriserenalalldate">
-            <span>2001-01-11</span>
+          <div>
+            <el-button icon="el-icon-star-off" v-if="!item.isKeep" size="mini" @click="collect(item)">收藏</el-button>
+            <el-button type="primary" icon="el-icon-star-on" v-if="item.isKeep" size="mini">已收藏</el-button>
           </div>
         </div>
-        <div class="entepriserenalall"></div>
-        <div class="entepriserenalall"></div>
-        <div class="entepriserenalall"></div>
       </div>
-      <div></div>
+    </div>
+    <div class="pagination">
+      <pagination :total="total" @getCurrentPage="getCurrentPage"></pagination>
+    </div>
+  </div>
     </div>
   </div>
 </template>
 
-  <script>
+
+<script>
+import { mapMutations, mapGetters } from "vuex";
+import pagination from "@/components/pagination"
 export default {
   data() {
     return {
-      pathIndex: 0,
-      isshow: true,
-      dd: true
+      searchText: "",
+      page: 1,
+      pageSize: 10,
+      list:[],
+      total:0
     };
   },
-  methods: {
-    can() {
-      this.isshow = true;
-    },
-    to() {
-      this.isshow = false;
+  components:{
+    pagination
+  },
+  computed: {
+    ...mapGetters(["getSeacherText"])
+  },
+  watch: {
+    getSeacherText() {
+      console.log(this.getSeacherText);
     }
+  },
+  methods: {
+    /**
+     * 取消收藏
+     */
+    unCollect(item) {
+
+    },
+    /**
+     * 收藏
+     */
+    collect(item) {
+      let obj = {
+        url: item.url,
+        titleZh: item.titleZh,
+        domain: item.domain,
+        created: item.created,
+        abstractZh: item.abstractZh,
+        languageTname: item.languageTname,
+        keywordsZh: JSON.stringify(item.keywordsZh),
+        uuid:item.uuid
+      }
+      this.$post('/yesskeep/',obj).then(res => {
+        console.log(res)
+        this.getPOList();
+      })
+    },
+    getCurrentPage(page){
+      console.log(page)
+      this.getPOList(page);
+    },
+    /**
+     * 外联
+     */
+    href(url){
+      window.open(url)
+    },
+    /**
+     * 获取检索信息
+     */
+    getPOList(pageNo) {
+      let obj = {
+        keyWord: this.getSeacherText,
+        pageSize: 10,
+        pageNo,
+        ysType: 0
+      };
+      this.$post("/yess/invoke",obj).then(res => {
+        console.log(res);
+        this.list = res.data.resultList
+        this.total = res.data.resultCount
+      });
+    }
+  },
+  created() {
+    this.getPOList(1);
   }
 };
 </script>
 
-  <style scoped>
+
+<style scoped type="text/scss" lang="scss">
 .dd {
   color: #557bf7;
   font-weight: 600;
@@ -101,85 +153,63 @@ export default {
 .enterpriseLL {
   margin-left: 25px;
 }
-.entepriserenal {
-  width: 96%;
-  height: 800px;
-  margin-left: 2%;
-  margin-right: 2%;
-  height: 300px;
-}
-.entepriserenalsource {
-  width: 100%;
-  height: 40px;
-  background: #ececf5;
-
-  line-height: 40px;
-}
-.entepriserenalsource-in {
-  color: #c1c3ce;
-  margin-left: 230px;
-}
-.entepriserenalsource-to {
-  color: #c1c3ce;
-  margin-left: 480px;
-}
-.entepriserenalsource-auto {
-  color: #c1c3ce;
-  margin-left: 200px;
-}
-.entepriserenalall {
-  padding-top: 20px;
-  width: 100%;
-  height: 200px;
-  border-bottom: 1px solid #f7f7fc;
-  position: relative;
-}
-.entepriserenalallTB {
-  position: absolute;
-  left: 10px;
-  width: 25px;
-  height: 200px;
-  line-height: 200px;
-}
-.entepriserenalallnm {
-  position: absolute;
-  left: 45px;
-  width: 150px;
-  height: 200px;
-  line-height: 200px;
-}
-.entepriserenalallWuHa {
-  position: absolute;
-  left: 199px;
-  height: 200px;
-  width: 500px;
-  border: none;
-}
-.entepriserenalallWuHa-nav {
-  position: absolute;
-  font-weight: bold;
-  font-size: 18px;
-  left: 30px;
-  top: 30px;
-  color: #838895;
-}
-.entepriserenalallFe {
-  position: absolute;
-  left: 30px;
-  top: 70px;
-  line-height: 170%;
-  color: #a6aab3;
-}
-.entepriserenalallZhican {
-  position: absolute;
-  left: 750px;
-  height: 200px;
-  line-height: 200px;
-}
-.entepriserenalalldate {
-  position: absolute;
-  left: 1020px;
-  height: 200px;
-  line-height: 200px;
+.pagination{
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+.publicOpinion {
+  width: 1200px;
+  margin: 0 auto;
+  background: #ffffff;
+  .box {
+    // margin-top: 20px;
+    .item {
+      margin: 0 30px;
+      padding: 20px 0;
+      border-bottom: 1px solid #edecf1;
+      color: #838895;
+      h3 {
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+      h3:hover{
+        color: #5180ff;
+      }
+      p:nth-child(2) {
+        color: #cad0dd;
+        font-size: 12px;
+        margin: 10px 0;
+      }
+      p:nth-child(3) {
+        font-size: 14px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        margin-bottom: 15px;
+      }
+      .i_footer {
+        display: flex;
+        justify-content: space-between;
+        .bord{
+          border: 1px solid #c26d10;
+          color: #c26d10;
+          border-radius: 4px;
+          padding: 2px 10px;
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+  .pagination{
+    width: 1200px;
+    margin: 0 auto;
+    text-align: center;
+    padding: 30px 0;
+    background-color: #fff;
+  }
 }
 </style>
